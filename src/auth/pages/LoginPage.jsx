c5a2286ba10f2d8deace30a3a4fa5f3c766ac2de
davidperezboza <david.perez@babelgroup.com>
2025-01-27 15:30:17 +0100

    Video 244

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,39 @@
+import { Grid2, TextField, Typography } from '@mui/material';
+
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Grid2 container
+           spacing={0} 
+           direction="column"
+           alignItems='center'
+           justifyContent="center"
+           sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
+    >
+      <Grid2 item
+        className='box-shadow'
+        xs={3}
+        sx={{backgroundColor: 'white', padding: 3, borderRadius: 2}}>
+          <Typography variant='h5' sx={{mb: 1}}>Login</Typography>
+          <form>
+            <Grid2 container>
+              <Grid2 item xs={12} sx={{mt: 2}}>
+                <TextField 
+                  label='Correo' 
+                  type='email' 
+                  placeholder='coreo@google.com'
+                  fullWidth/>
+              </Grid2>
+
+              <Grid2 item xs={12} sx={{mt: 2}}>
+                <TextField 
+                  label='Contraseña' 
+                  type='password' 
+                  placeholder='Contraseña'
+                  fullWidth/>
+              </Grid2>
+            </Grid2>
+          </form>
+      </Grid2>
+    </Grid2>
   )
 }
