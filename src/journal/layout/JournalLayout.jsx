bd616a16fe5056d7a015a204c95088ec96f009c4
davidperezboza <david.perez@babelgroup.com>
2025-01-27 19:59:11 +0100

    Video 249

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,12 @@
-import { Box } from "@mui/material"
+import { Box } from '@mui/material';
+import { NavBar } from '../components/';
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({children}) => {
     return (
         <Box sx={{display: 'flex'}}>
-            {/* NavBar drawerWidth*/}
+            <NavBar drawerWidth={drawerWidth}/>
 
             {/* Sidebar drawerWidth*/}
 
