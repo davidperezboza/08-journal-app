748596ce5e7882b5ebecbe3337694918ddacc529
davidperezboza <david.perez@babelgroup.com>
2025-01-27 16:19:26 +0100

    Video 245

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,39 +1,60 @@
-import { Grid2, TextField, Typography } from '@mui/material';
+import {Link as RouterLink} from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 
 export const LoginPage = () => {
   return (
-    <Grid2 container
+    <Grid container
            spacing={0} 
            direction="column"
            alignItems='center'
            justifyContent="center"
            sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
     >
-      <Grid2 item
+      <Grid item
         className='box-shadow'
         xs={3}
         sx={{backgroundColor: 'white', padding: 3, borderRadius: 2}}>
           <Typography variant='h5' sx={{mb: 1}}>Login</Typography>
           <form>
-            <Grid2 container>
-              <Grid2 item xs={12} sx={{mt: 2}}>
+            <Grid container>
+              <Grid item xs={12} sx={{mt: 2}}>
                 <TextField 
                   label='Correo' 
                   type='email' 
                   placeholder='coreo@google.com'
                   fullWidth/>
-              </Grid2>
+              </Grid>
 
-              <Grid2 item xs={12} sx={{mt: 2}}>
+              <Grid item xs={12} sx={{mt: 2}}>
                 <TextField 
                   label='Contraseña' 
                   type='password' 
                   placeholder='Contraseña'
                   fullWidth/>
-              </Grid2>
-            </Grid2>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+              <Grid item xs={12} sm={6}> 
+                <Button variant='contained' fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}> 
+                <Button variant='contained' fullWidth>
+                  <Google />
+                  <Typography sx={{ml: 1}}>Google</Typography>
+                </Button>
+              </Grid>
+              <Grid container={RouterLink} direction='row' justifyContent='end'>
+                <Link color='inherit' to='/auth/register'>
+                  Crear una cuenta
+                </Link>
+              </Grid>
+            </Grid>
           </form>
-      </Grid2>
-    </Grid2>
+      </Grid>
+    </Grid>
   )
 }
