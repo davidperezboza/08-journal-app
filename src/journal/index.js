e1b674a9b0e1892078e18dc3e7a18758fb58fc35
davidperezboza <david.perez@babelgroup.com>
2025-01-27 21:25:37 +0100

    Video 251

--- src/journal/index.js
+++ src/journal/index.js
@@ -1,2 +1,5 @@
 export * from './pages';
 export * from './routes';
+export * from './components';
+export * from './layout';
+export * from './views';
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,12 @@
-import { Typography } from "@mui/material";
-import { JournalLayout } from "../layout/";
+import { JournalLayout } from '../layout/';
+import { NothingSelectedView } from '../views/';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Reprehenderit fugiat sint esse adipisicing occaecat non ut occaecat consectetur nostrud quis. Aliqua ad culpa tempor cupidatat aute eiusmod esse ullamco. Nulla ut amet ad nisi non ad ex reprehenderit aliqua occaecat. Exercitation aliqua id aliqua tempor proident sunt.</Typography>
+      {/*<Typography>Reprehenderit fugiat sint esse adipisicing occaecat non ut occaecat consectetur nostrud quis. Aliqua ad culpa tempor cupidatat aute eiusmod esse ullamco. Nulla ut amet ad nisi non ad ex reprehenderit aliqua occaecat. Exercitation aliqua id aliqua tempor proident sunt.</Typography>*/}
       
-      {/** NothingSelected */}
+      < NothingSelectedView />
       {/** NoteView */}
     </JournalLayout>
   )
